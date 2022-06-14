700d3e41eaf5164aabafb9a483e6a1fa9527b180
pmiskicA <106012705+pmiskicA@users.noreply.github.com>
2022-06-14 16:34:18 +0200

    New payment gateway

--- project-source-code/api-gateway/APIGateway/src/main/java/com/taxi/now/APIGateway/PaymentRestController.java
+++ project-source-code/api-gateway/APIGateway/src/main/java/com/taxi/now/APIGateway/PaymentRestController.java
@@ -7,25 +7,25 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-//We are creating payment rest controller to show payment
 @RestController
 @RequestMapping("/api/")
 public class PaymentRestController {
 
-    @PostMapping("/payment_info")
-    @ResponseBody
-    public List getPaymentInfo(@RequestParam(name = "payment_id") List<Integer> payment_id, @RequestParam(name="card_number") List<Integer> card_number, @RequestParam(name="card_holder") List<String> card_holder, @RequestParam(name="expiration_year") List<Integer> expiration_year, @RequestParam(name="expiration_month") List<Integer> expiration_month, @RequestParam(name="CVV") List<Integer> CVV) {
-        //card_number, card_holder, expiration_year, expiration_month, CVV.
-        List list = new ArrayList<>();
-        list.add(payment_id);
-        list.add(card_number);
-        list.add(card_holder);
-        list.add(expiration_month);
-        list.add(expiration_year);
-        list.add(CVV);
-        System.out.println(payment_id);
-
-        return list;
+    @RequestMapping(value = "/payment_info", method = RequestMethod.POST )
+    public void getPaymentInfo(@RequestParam Map<String, String> requestParams)
+    throws Exception {
+        String card_holder = requestParams.get("card_holder");
+        String card_number = requestParams.get("card_number");
+        String expiration_month = requestParams.get("expiration_month");
+        String expiration_year = requestParams.get("expiration_year");
+        String CVV = requestParams.get("CVV")
+        System.out.println("Card holder: " +  card_number +
+                "\n Card number :" + card_number +
+                "\n Expiration month : "+ expiration_month + "\n Expiration year:" + expiration_year + "\n CVV:" + CVV);
+        
+       
+        
+        return ;
     }
 
 
