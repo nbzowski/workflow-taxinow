e115d9a75d2566a6e3c96babca00d6ce07fcfc27
Nicholas Arthur Bzowski <e12141086@student.tuwien.ac.at>
2022-06-15 21:12:18 +0200

    Finished Process Ride Request

--- project-source-code/api-gateway/APIGateway/src/main/java/com/taxi/now/APIGateway/CreateRide.java
+++ project-source-code/api-gateway/APIGateway/src/main/java/com/taxi/now/APIGateway/CreateRide.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web
 
 import java.util.List;
 import java.util.Map;
